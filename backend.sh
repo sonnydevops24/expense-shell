a74644567e3cf73d5975fbd674e1597f16824836
new commit <ebhos4real@yahoo.com>
2024-05-21 00:13:06 -0400

    backend script added

--- backend.sh
+++ backend.sh
@@ -1,16 +1,19 @@
 dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:20 -y
 dnf install nodejs -y
+useradd expense
 cp backend.service /etc/systemd/system/backend.service
+mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
 dnf install mysql -y
 mysql -h 172.31.19.91 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
-dnf update -y
+
 
 
