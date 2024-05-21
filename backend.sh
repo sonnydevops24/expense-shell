1f4a33eda50148ed224cd55883077255d41bebc4
new commit <ebhos4real@yahoo.com>
2024-05-21 00:02:38 -0400

    Added backend update

--- backend.sh
+++ backend.sh
@@ -5,7 +5,6 @@ cp backend.service /etc/systemd/system/backend.service
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
-npm install
 
 dnf install mysql -y
 mysql -h 172.31.19.91 -uroot -pExpenseApp@1 < /app/schema/backend.sql
